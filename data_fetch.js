8c843df8c866cfe0b4fa4321f2506831ca620e47
Njokuocha Francis <145536207+Njokuocha@users.noreply.github.com>
2024-08-05 22:49:53 +0100

    Add files via upload

---
+++ data_fetch.js
@@ -0,0 +1,25 @@
+// website: https://developer.accuweather.com
+// api authentication key
+const authKey = 'AKEjOpAHjQDyHizRHkSGfK2eLotjKax3';
+// fetching city weather condition using city key 
+const fetchCityWeather = async (citykey) =>{
+    let resourceTarget = `http://dataservice.accuweather.com/currentconditions/v1/${citykey}`;
+    let query = `?apikey=${authKey}&details=true`;
+    resourceTarget += query;
+    const city = await fetch(resourceTarget);
+    const cityInfo = await city.json();
+    return cityInfo[0];
+}
+// fetching city details with "Key" as target 
+const fetchCityInfo = async (cityname) =>{
+    let resourceTarget = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    let query = `?apikey=${authKey}&q=${cityname}`;
+    resourceTarget += query;
+    const cities = await fetch(resourceTarget);
+    const citiesInfo = await cities.json();
+    return citiesInfo[0];
+}
+
+// Programmed by Njokuocha Francis
+// Completed on Mon 5th August 2024 by 10:20pm
+// Face your fears and get rid of it 
